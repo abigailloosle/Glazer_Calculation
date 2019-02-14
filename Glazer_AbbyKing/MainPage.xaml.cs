9b10af88f09984e6d171cda7e95a244fb1060a06
Abby Loosle <Abby Loosle@DESKTOP-2EIM5T9>
2019-02-14 08:08:13 -0800

    Add Galzer

--- Glazer_AbbyKing/MainPage.xaml.cs
+++ Glazer_AbbyKing/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,54 @@ namespace Glazer_AbbyKing
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+            double width = 0;
+            double height = 0;
+            double slide;
+
+            // checked if width is a number
+            if(double.TryParse(txtWidth.Text, out double number)){
+                width = double.Parse(txtWidth.Text);
+            } else {
+                txtWidth.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtWidth.Text = "Please enter a number.";
+            };
+
+            // checked if height is a number
+            if (double.TryParse(txtHeight.Text, out double number2))
+            {
+                height = double.Parse(txtHeight.Text);
+            }
+            else
+            {
+                txtHeight.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtHeight.Text = "Please enter a number.";
+            };
+
+            slide = double.Parse(txtSlider.Text);
+
+            // only calculate if values other than 0 are entered
+            if (!(width == 0 && height == 0 && slide == 0))
+            {
+                txtLength.Text = (2 * (width + height) * 3.25 * slide).ToString();
+                txtArea.Text = (2 * (width + height) * slide).ToString();
+                txtDate.Text = (System.DateTime.Now).ToString();
+            } else
+            {
+                txtWidth.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtHeight.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtSlider.BorderBrush = new SolidColorBrush(Colors.Red);
+                txtError.Text = "All fields must have a value. Please try again.";
+            }
+
+        }
+
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            txtSlider.Text = ((Slider)sender).Value.ToString();
         }
     }
 }
